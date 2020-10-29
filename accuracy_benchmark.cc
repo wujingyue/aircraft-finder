c4025976f8a0699f90a4457f7f7d775e1021fda2
Jingyue Wu <wujingyue@gmail.com>
2020-10-28 20:27:53 -0700

    Print the distribution of the number of guesses.

--- accuracy_benchmark.cc
+++ accuracy_benchmark.cc
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <numeric>
 #include <tuple>
 
 #include "aircraft_finder.h"
@@ -12,6 +13,55 @@ using namespace std;
 void PrintUsage(const char* exec_name) {
   cerr << "Usage: " << exec_name << " -r rows -c cols -n aircrafts" << endl;
 }
+
+class Histogram {
+ public:
+  void AddNumGuesses(const int num_guesses) {
+    if (num_guesses >= static_cast<int>(num_games_.size())) {
+      num_games_.resize(num_guesses + 1);
+    }
+    num_games_[num_guesses]++;
+  }
+
+  void Print() const {
+    cout << "Average = " << Average() << endl;
+    cout << "Median = " << Median() << endl;
+    cout << "Worst = " << Worst() << endl;
+    for (int num_guesses = 0, size = num_games_.size(); num_guesses < size;
+         num_guesses++) {
+      printf("%3d: ", num_guesses);
+      cout << string(num_games_[num_guesses], '#') << endl;
+    }
+  }
+
+ private:
+  float Average() const {
+    int count = accumulate(num_games_.begin(), num_games_.end(), 0);
+    int sum = 0;
+    for (int num_guesses = 0, size = num_games_.size(); num_guesses < size;
+         num_guesses++) {
+      sum += num_guesses * num_games_[num_guesses];
+    }
+    return static_cast<float>(sum) / count;
+  }
+
+  int Median() const {
+    int count = accumulate(num_games_.begin(), num_games_.end(), 0);
+    int sum = 0;
+    for (int num_guesses = 0, size = num_games_.size(); num_guesses < size;
+         num_guesses++) {
+      sum += num_games_[num_guesses];
+      if (sum >= count / 2) {
+        return num_guesses;
+      }
+    }
+    return -1;
+  }
+
+  int Worst() const { return num_games_.size() - 1; }
+
+  vector<int> num_games_;
+};
 
 int main(int argc, char* argv[]) {
   int rows = 0;
@@ -42,25 +92,31 @@ int main(int argc, char* argv[]) {
   }
 
   AircraftGenerator generator(rows, cols, num_aircrafts);
-  vector<vector<Color>> board = generator.Generate();
+  srand(1229);
 
-  AircraftFinder finder(rows, cols, num_aircrafts);
-  int num_remaining_aircrafts = num_aircrafts;
-  int num_guesses = 0;
-  while (num_remaining_aircrafts > 0) {
-    num_guesses++;
+  Histogram histogram;
+  for (int i = 0; i < 100; i++) {
+    vector<vector<Color>> board = generator.Generate();
 
-    int x;
-    int y;
-    tie(x, y) = finder.GetCellToBomb(false);
-    cerr << x << ' ' << y << ' ' << board[x][y] << endl;
-    finder.SetColor(x, y, board[x][y]);
-    if (board[x][y] == kRed) {
-      num_remaining_aircrafts--;
+    AircraftFinder finder(rows, cols, num_aircrafts);
+    int num_remaining_aircrafts = num_aircrafts;
+    int num_guesses = 0;
+    while (num_remaining_aircrafts > 0) {
+      num_guesses++;
+
+      int x;
+      int y;
+      tie(x, y) = finder.GetCellToBomb(false);
+      finder.SetColor(x, y, board[x][y]);
+      if (board[x][y] == kRed) {
+        num_remaining_aircrafts--;
+      }
     }
+
+    histogram.AddNumGuesses(num_guesses);
   }
 
-  cout << "Number of guesses = " << num_guesses << endl;
+  histogram.Print();
 
   return 0;
 }
